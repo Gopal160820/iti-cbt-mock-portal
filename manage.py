1981f00139fa5c7c1e1c608dbe43ac3613f0e1d7
Gopal Bhunia <gopalbhunia010@gmail.com>
2026-05-08 12:37:44 +0530

    clean manage.py fix

--- manage.py
+++ manage.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
 
 
 def main():
     """Run administrative tasks."""
-<<<<<<< HEAD
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cbt_exam.settings')
-=======
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cbt_system.settings')
->>>>>>> ce273ed71ba1f53413829f679ae12dd266591d37
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -19,6 +17,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
