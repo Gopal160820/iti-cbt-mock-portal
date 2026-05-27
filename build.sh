1c948cd6f1d3a4aa6da861634ec23d3dceae4e95
Gopal Bhunia <gopalbhunia010@gmail.com>
2026-05-27 22:26:10 +0530

    create admin user

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 
 pip install -r requirements.txt
 
-python manage.py migrate
+python manage.py migrate --noinput
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@gmail.com', 'admin')" | python manage.py shell
+
 python manage.py collectstatic --noinput
